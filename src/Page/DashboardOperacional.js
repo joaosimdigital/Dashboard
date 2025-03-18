740141d3038c6973e9c2763eccea725338816e98
joaosimdigital <jsousa@sc.sim.digital>
2025-03-18 14:33:20 -0300

    Atualização 7

--- src/Page/DashboardOperacional.js
+++ src/Page/DashboardOperacional.js
@@ -9,8 +9,35 @@ export default function DashboardOperacional() {
             <div>
 
                 <div className='row-div-instalacao-manutencao'>
-
+                    
                     <div className='div-card-dados-principais'>
+                            <h1 className='h1-titulo-card-dados-principais'>Instalações</h1>
+                        <div className='row-div-card-dados-principais'>
+                      
+                            <div className='div-card-dados-principais-dados'>
+                               
+                                <div className='card-dados-principais-dados'>
+                                        <h1 className='h1-card-dados-principais-dados'>SC</h1>
+                                        <h1 className='h2-card-dados-principais-dados'>145</h1>
+                                </div>
+                                <div className='card-dados-principais-dados'>
+                                        <h1 className='h1-card-dados-principais-dados'>RS</h1>
+                                        <h1 className='h2-card-dados-principais-dados'>145</h1>
+                                </div>
+                            </div>
+
+                            <div className='div-card-dados-principais-dados-diaria'>
+                                    <h1 className='h1-div-card-dados-principais-dados-diaria'>Diária</h1>
+                                    <h1>0</h1>
+
+
+                                </div>
+
+                            <div>
+
+                            </div>
+
+                        </div>
 
                     </div>
 
