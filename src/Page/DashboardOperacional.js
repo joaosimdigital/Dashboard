05a03435cd9bbd4b7233f870b11fcfcf6dfbf3ea
joaosimdigital <jsousa@sc.sim.digital>
2025-03-18 15:20:13 -0300

    Atualização 8

--- src/Page/DashboardOperacional.js
+++ src/Page/DashboardOperacional.js
@@ -14,6 +14,9 @@ export default function DashboardOperacional() {
                             <h1 className='h1-titulo-card-dados-principais'>Instalações</h1>
                         <div className='row-div-card-dados-principais'>
                       
+
+                      <div className='body-div-card-dados-principais-dados'>
+                            <h1 className='h1-body-div-card-dados-principais'>Mês atual</h1>
                             <div className='div-card-dados-principais-dados'>
                                
                                 <div className='card-dados-principais-dados'>
@@ -24,14 +27,33 @@ export default function DashboardOperacional() {
                                         <h1 className='h1-card-dados-principais-dados'>RS</h1>
                                         <h1 className='h2-card-dados-principais-dados'>145</h1>
                                 </div>
+                            
+
+                           
                             </div>
 
+                            <div className='div-meta-card-dados-principais'>
+                                <h1 className='h1-meta-card-dados-principais'>Meta Mês</h1>
+                                <h1  className='h2-meta-card-dados-principais'>0</h1>
+                            </div>
+
+                            </div>
+
+                            <div className='body-div-card-dados-principais-dados'>
+                         
                             <div className='div-card-dados-principais-dados-diaria'>
                                     <h1 className='h1-div-card-dados-principais-dados-diaria'>Diária</h1>
-                                    <h1>0</h1>
+                                    <h1 className='h2-div-card-dados-principais-dados-diaria'>0</h1>
 
 
                                 </div>
+                                
+
+                                <div className='div-meta-card-dados-principais'>
+                                <h1 className='h1-meta-card-dados-principais'>Meta / Média</h1>
+                                <h1  className='h2-meta-card-dados-principais'>0</h1>
+                                </div>
+                            </div>
 
                             <div>
 
@@ -43,8 +65,58 @@ export default function DashboardOperacional() {
 
 
                     <div className='div-card-dados-principais'>
+                            <h1 className='h1-titulo-card-dados-principais'>Manutenção</h1>
+                        <div className='row-div-card-dados-principais'>
+                      
+
+                      <div className='body-div-card-dados-principais-dados'>
+                            <h1 className='h1-body-div-card-dados-principais'>Mês atual</h1>
+                            <div className='div-card-dados-principais-dados'>
+                               
+                                <div className='card-dados-principais-dados'>
+                                        <h1 className='h1-card-dados-principais-dados'>SC</h1>
+                                        <h1 className='h2-card-dados-principais-dados'>145</h1>
+                                </div>
+                                <div className='card-dados-principais-dados'>
+                                        <h1 className='h1-card-dados-principais-dados'>RS</h1>
+                                        <h1 className='h2-card-dados-principais-dados'>145</h1>
+                                </div>
+                            
+
+                           
+                            </div>
+
+                            <div className='div-meta-card-dados-principais'>
+                                <h1 className='h1-meta-card-dados-principais'>Meta Mês</h1>
+                                <h1  className='h2-meta-card-dados-principais'>0</h1>
+                            </div>
+
+                            </div>
+
+                            <div className='body-div-card-dados-principais-dados'>
+                         
+                            <div className='div-card-dados-principais-dados-diaria'>
+                                    <h1 className='h1-div-card-dados-principais-dados-diaria'>Diária</h1>
+                                    <h1 className='h2-div-card-dados-principais-dados-diaria'>0</h1>
+
+
+                                </div>
+                                
+
+                                <div className='div-meta-card-dados-principais'>
+                                <h1 className='h1-meta-card-dados-principais'>Meta / Média</h1>
+                                <h1  className='h2-meta-card-dados-principais'>0</h1>
+                                </div>
+                            </div>
+
+                            <div>
+
+                            </div>
+
+                        </div>
 
                     </div>
+
 
                     </div>
 
