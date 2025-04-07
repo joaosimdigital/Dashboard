5d6738c5e42a8ddf8219322a532968eafc96d8b3
joaosimdigital <jsousa@sc.sim.digital>
2025-04-07 09:11:46 -0300

    Atualização 48

--- src/Page/DashboardOsTicket.js
+++ src/Page/DashboardOsTicket.js
@@ -1,9 +1,76 @@
 import React, { Component } from 'react'
 
+
+import '../CSS/DashboardOsTicket.css'
+
+import logobranca from '../Images/logobrnaca.png'
+
+
 export class DashboardOsTicket extends Component {
   render() {
     return (
-      <div>DashboardOsTicket</div>
+      <div>
+
+        <div className='div-header-osticket'>
+
+          <h1 style={{marginLeft: 20}}></h1>
+
+            <h1 className='titulo-div-header-osticket'>TI<h1 className='subtitulo-div-header-osticket'>CKET's</h1></h1>
+            <img className='logo-div-header-osticket' src={logobranca}/>
+        </div>
+
+        <div className='div-metricas-osticket'>
+           
+            <div className='card1-metricas-osticket'>
+              <h1 className='card-titulo-metricas'>Total</h1>
+              <h1  className='card-subtitulo-metricas'>0</h1>
+            </div>
+
+
+            <div className='card2-metricas-osticket'>
+              <h1 className='card-titulo-metricas'>Abertos</h1>
+              <h1  className='card-subtitulo-metricas'>0</h1>
+            </div>
+
+            <div  className='card3-metricas-osticket'>
+              <h1 className='card-titulo-metricas'>Respondidos</h1>
+              <h1  className='card-subtitulo-metricas'>0</h1>
+            </div>
+
+
+            <div  className='card4-metricas-osticket'>
+              <h1 className='card-titulo-metricas'>Resolvidos</h1>
+              <h1  className='card-subtitulo-metricas'>0</h1>
+            </div>
+
+
+
+
+        </div>
+
+
+        <div className='div-graficos'>
+
+          <div className='card1-div-graficos'>
+            <div>
+            <h1>Abertos x Mês</h1>
+            </div>
+          </div>
+
+
+          <div className='card2-div-graficos'>
+            <h1>TMS</h1>
+          </div>
+
+
+          <div className='card3-div-graficos'>
+            <h1>Gráficos</h1>
+          </div>
+
+
+        </div>
+
+      </div>
     )
   }
 }
